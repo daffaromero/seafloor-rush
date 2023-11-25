c1fbd59d1ea7ef93c87e81181fb0bddeb9c21815
daffaromero <daffaromero@mail.ugm.ac.id>
2023-11-25 15:49:55 +0700

    New scene with player + joystick

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody2D _rigidbody;
+    private float _speed = 15f;
+
+    private void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    public void OnMove(InputAction.CallbackContext ctx)
+    {
+        _rigidbody.velocity = ctx.ReadValue<Vector2>() * _speed;
+    }
+}
