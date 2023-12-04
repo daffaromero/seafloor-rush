4b1a5d6aab202d94b92489f1209c75387c4d2e3c
reiiiofsunshine1 <75015733+reiiiofsunshine1@users.noreply.github.com>
2023-12-04 16:34:57 +0700

    [added and modified] MUSIC AND GRAPHICS
    telolet

---
+++ Assets/AudioManager.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    [Header("----- Audio Source -----")]
+    // Start is called before the first frame update
+    [SerializeField] AudioSource musicSource;
+    [SerializeField] AudioSource SFXSource;
+
+    [Header("----- Audio Clip -----")]
+    public AudioClip background;
+    public AudioClip death;
+
+    private void Start()
+    {
+        musicSource.clip = background;
+        musicSource.Play();
+    }
+
+    public void PlaySFX(AudioClip clip)
+    {
+        SFXSource.PlayOneShot(clip);
+    }
+}
