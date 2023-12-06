5bf90b03def482ee5280ac089c57e08468a06032
reiiiofsunshine1 <75015733+reiiiofsunshine1@users.noreply.github.com>
2023-12-06 17:26:37 +0700

    [fixed] handler playerprefs
    hiho

--- Assets/Scripts/Audio/VolumeSettings.cs
+++ Assets/Scripts/Audio/VolumeSettings.cs
@@ -6,31 +6,43 @@ using UnityEngine.Audio;
 
 public class VolumeSettings : MonoBehaviour
 {
-    // Start is called before the first frame update
-    
     [SerializeField] Slider volumeSlider;
-    
+
+    private Transform handlerTransform; // Reference to the knob transform
+
     void Start()
     {
-        if(!PlayerPrefs.HasKey("musicVolume"))
+        handlerTransform = volumeSlider.transform.Find("Handler");
+
+        if (!PlayerPrefs.HasKey("musicVolume"))
         {
             PlayerPrefs.SetFloat("musicVolume", 1);
             Load();
         }
+        else
+        {
+            Load(); // Load the saved volume
+            volumeSlider.onValueChanged.AddListener(delegate { Save(); }); // Listen for changes
+        }
     }
 
-    // Update is called once per frame
     public void ChangeVolume()
     {
         AudioListener.volume = volumeSlider.value;
     }
+
     private void Load()
     {
         volumeSlider.value = PlayerPrefs.GetFloat("musicVolume");
+        if (handlerTransform != null)
+        {
+            handlerTransform.localPosition = new Vector3(volumeSlider.value * volumeSlider.GetComponent<RectTransform>().rect.width, 0, 0);
+        }
     }
 
     private void Save()
     {
         PlayerPrefs.SetFloat("musicVolume", volumeSlider.value);
+        PlayerPrefs.Save(); // Save the volume setting
     }
 }
