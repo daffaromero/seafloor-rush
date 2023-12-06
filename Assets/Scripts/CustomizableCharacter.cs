3242123cab7b463b8c1fe92cc324f63d4b991fbf
reiiiofsunshine1 <75015733+reiiiofsunshine1@users.noreply.github.com>
2023-12-06 16:14:54 +0700

    [Added] Skin change and added new skin
    angry octopus >:[

---
+++ Assets/Scripts/CustomizableCharacter.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CustomizableCharacter : MonoBehaviour
+{
+    public int skinNr;
+    public SkinnedMeshRenderer[] skins;
+    SpriteRenderer spriteRenderer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void SkinChoice()
+    {
+        if (spriteRenderer.sprite.name.Contains("Skin1"));
+        
+    }
+}
